d22cc905595155a627dccff5cddecb5e07df6ab4
vvaditya23 <vvaditya23@icloud.com>
2023-05-17 12:27:50 +0530

    Initial Commit

---
+++ Instagram_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram_clone
+//
+//  Created by ヴィヤヴャハレ・アディティア on 17/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
